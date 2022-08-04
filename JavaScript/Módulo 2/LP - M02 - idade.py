bbfd84f8e270781e643c331237bfecd85f25730e
Marilia Santiago <marilia.aline18@gmail.com>
2022-08-04 17:26:43 -0300

    LP M02 Idade

--- JavaScript/Módulo 2/LP - M02 - idade.py
+++ JavaScript/Módulo 2/LP - M02 - idade.py
@@ -4,7 +4,11 @@ while True:
     try:
 
         print("=======Informe seus dados=======\n")
-        nome = str(input("Nome: "))
+        nome = input("Nome: ")
+
+        if(nome == ''):
+            print("\033[0;31mErro! O campo Nome deve ser preenchido.\033[m")
+            continue
 
         print("Data de Nascimento")
         dia_nasc = int(input("Dia: "))
@@ -21,14 +25,14 @@ while True:
 
             if(mes_nasc > mes_atual or (mes_nasc == mes_atual and dia_nasc > dia_atual)):
                 idade = idade - 1
+
+            print("\n=======Dados Informados=======")
             print(f"\nNome: {nome}\nIdade: {idade} anos")
             print("\nPrograma Finalizado")
 
             break
-        else:
-            raise Exception("\nData inválida, digite uma data válida!")
     except:
-        print("\nErro! Preencha os campos corretamente!")
+        print("\033[0;31m\nErro! Informe uma data válida!\033[m")
 
 
 
